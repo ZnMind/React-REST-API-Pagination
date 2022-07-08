e5f008f31f1623a8549fcc91565fb7fcb4efbe99
DKProtocol <damann1072@gmail.com>
2022-07-07 19:09:59 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Pagination from './Pagination';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  async componentDidMount() {
+    //this.loadCoinDesk()
+    await this.loadCoinCap()
+  }
+
+  async loadCoinDesk() {
+    let data = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = await data.json()
+    console.log(data)
+    this.setState({
+      time: data.time.updated,
+      chart: data.chartName,
+      code: data.bpi.USD.code,
+      rate: data.bpi.USD.rate_float
+    })
+  }
+
+  async loadCoinCap() {
+    let data = await fetch(`https://api.coincap.io/v2/assets`)
+    data = await data.json()
+    this.setState({ currencies: data.data })
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currencies: [],
+      prices: [],
+      time: null,
+      chart: null,
+      code: null,
+      rate: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='d-flex justify-content-between'>
+          <Pagination
+            data={this.state.currencies}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pagination.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import './Pagination.css'
+
+const Pagination = data => {
+    const [pages] = useState(Math.round(data.data.length / 10))
+    const [currentPage, setCurrentPage] = useState(1);
+
+    function goToNextPage() {
+        setCurrentPage((page) => page + 1);
+    }
+
+    function goToPreviousPage() {
+        setCurrentPage((page) => page - 1);
+    }
+
+    function changePage(event) {
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
+    }
+
+    const getPaginatedData = () => {
+        const startIndex = currentPage * 10 - 10;
+        const endIndex = startIndex + 10;
+        return data.data.slice(startIndex, endIndex);
+    };
+
+    const getPaginationGroup = () => {
+        let start = Math.floor((currentPage - 1) / 10) * 10;
+        return new Array(10).fill().map((_, idx) => start + idx + 1);
+    };
+
+    return (
+        <div>
+            <h1 className="text-align-center">Top 100</h1>
+            <div className="header-box">
+                <div className="box">
+                    <h5 className="header-row">Rank</h5>
+                    <h5 className="header-row">Symbol</h5>
+                    <h5 className="header-row">USD</h5>
+                    <h5 className="header-row">24h Change</h5>
+                </div>
+            </div>
+
+            <div className="dataContainer">
+
+                {getPaginatedData().map((data, index) => (
+                    <div key={index} className="box">
+                        <div className="column">
+                            {data.rank}
+                        </div>
+                        <div className="column">
+                            {data.symbol}
+                        </div>
+                        <div className="columnFix">
+                            ${data.priceUsd > 1 ?
+                                Math.floor(data.priceUsd * 100) / 100 :
+                                data.priceUsd < 0.0001 ?
+                                    Math.floor(data.priceUsd * 10000000) / 10000000 :
+                                    Math.floor(data.priceUsd * 10000) / 10000}
+                        </div>
+                        <div className="column">
+                            {data.changePercent24Hr > 0 ?
+                                <span className='text-success'> {Math.floor(data.changePercent24Hr * 100) / 100}%</span> :
+                                <span className='text-danger'> {Math.floor(data.changePercent24Hr * 100) / 100}%</span>
+                            }
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="pagination">
+                {/* previous button */}
+                <button
+                    onClick={goToPreviousPage}
+                    className={`prev ${currentPage === 1 ? 'disabled' : ''}`}
+                >
+                    prev
+                </button>
+
+                {/* show page numbers */}
+                {getPaginationGroup().map((item, index) => (
+                    <button
+                        key={index}
+                        onClick={changePage}
+                        className={`paginationItem ${currentPage === item ? 'active' : null}`}
+                    >
+                        <span>{item}</span>
+                    </button>
+                ))}
+
+                {/* next button */}
+                <button
+                    onClick={goToNextPage}
+                    className={`next ${currentPage === pages ? 'disabled' : ''}`}
+                >
+                    next
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Pagination;
